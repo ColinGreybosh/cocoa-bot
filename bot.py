912e4ad3a267d9ea149cd4fd0f2a2019a97be438
Colin Greybosh <greybosh@mit.edu>
2020-03-17 05:08:26 -0400

    Reformating strings

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import os
 import re
 import sqlite3
@@ -14,10 +13,10 @@ from discord.ext import commands
 load_dotenv()
 
 # database rows have values of (id int, count int)
-COCOA_REGEX = re.compile('[cC]+[oO]+[cC]+[oO]+[aA]+')
-COCOA_DB = os.getenv('COCOA_DB')
-COCOA_TABLE = os.getenv('COCOA_TABLE')
-COCOA_CHANNEL = os.getenv('COCOA_CHANNEL')
+COCOA_REGEX = re.compile("[cC]+[oO]+[cC]+[oO]+[aA]+")
+COCOA_DB = os.getenv("COCOA_DB")
+COCOA_TABLE = os.getenv("COCOA_TABLE")
+COCOA_CHANNEL = os.getenv("COCOA_CHANNEL")
 
 VERBOSE = False
 
@@ -29,11 +28,11 @@ async def on_ready():
     conn = sqlite3.connect(COCOA_DB)
     c = conn.cursor()
     users_to_add = set([member.id for member in bot.guilds.pop().members])
-    for row in c.execute(f'SELECT * FROM {COCOA_TABLE}'):
+    for row in c.execute(f"SELECT * FROM {COCOA_TABLE}"):
         user_id, count = row
         users_to_add.remove(user_id)
     for user_id in users_to_add:  # Add any new guild members to the database
-        c.execute(f'INSERT INTO {COCOA_TABLE} VALUES ({user_id}, 0)')
+        c.execute(f"INSERT INTO {COCOA_TABLE} VALUES ({user_id}, 0)")
     conn.commit()
     conn.close()
 
@@ -42,11 +41,10 @@ async def on_ready():
         saturday = 5
         weekday, hour, minute, second = get_weekday_and_hms()
         if VERBOSE:
-            print(f'{weekday} {hour} {minute} {second}')
-        if weekday != friday and weekday != saturday:
-            if hour == 23 and minute == 0 and second == 0:
-                cocoa_channel = bot.get_channel(COCOA_CHANNEL)
-                await cocoa_channel.send('☕ COOOOOOOOOOOCOOOOOOOOOOOOOOOOAAAAAAAA!!! ☕')
+            print(f"{weekday} {hour} {minute} {second}")
+        if weekday != friday and weekday != saturday and hour == 23 and minute == 0 and second == 0:
+            cocoa_channel = bot.get_channel(COCOA_CHANNEL)
+            await cocoa_channel.send("☕ COOOOOOOOOOOCOOOOOOOOOOOOOOOOAAAAAAAA!!! ☕")
         await asyncio.sleep(1)
 
 
@@ -55,10 +53,10 @@ async def on_member_join(ctx: commands.Context, member: Member):
     conn = sqlite3.connect(COCOA_DB)
     c = conn.cursor()
     joined_user_id = member.id
-    for user_id in c.execute(f'SELECT user_id FROM {COCOA_TABLE}'):
+    for user_id in c.execute(f"SELECT user_id FROM {COCOA_TABLE}"):
         if user_id == joined_user_id:
             return
-    c.execute(f'INSERT INTO {COCOA_TABLE} VALUES ({joined_user_id}, 0)')
+    c.execute(f"INSERT INTO {COCOA_TABLE} VALUES ({joined_user_id}, 0)")
     conn.commit()
     conn.close()
 
@@ -66,7 +64,7 @@ async def on_member_join(ctx: commands.Context, member: Member):
 @bot.command(name='cocoa', help='Yells Cocoa! into your text channel.')
 async def on_message(ctx: commands.Context):
     """Yells cocoa into your text channel."""
-    await ctx.send('☕ COOOOOOOOOOOCOOOOOOOOOOOOOOOOAAAAAAAA!!! ☕')
+    await ctx.send("☕ COOOOOOOOOOOCOOOOOOOOOOOOOOOOAAAAAAAA!!! ☕")
 
 
 @bot.command(name='cocoa-count', help='Report the amount of times this @\'ed user has said \'cocoa\'')
@@ -82,7 +80,7 @@ async def on_message(ctx: commands.Context, user: str):
         user_id = int(user[3:len(user) - 1])
         try:
             display_name = get_display_name_by_id(bot, user_id)
-            c.execute(f'SELECT count FROM {COCOA_TABLE} WHERE user_id={user_id}')
+            c.execute(f"SELECT count FROM {COCOA_TABLE} WHERE user_id={user_id}")
             cocoa_count = c.fetchone()[0]
 
             await ctx.send(f'{display_name} has said COCOA on this server {cocoa_count} '
@@ -119,7 +117,7 @@ async def on_message(message: Message):
 
     conn = sqlite3.connect(COCOA_DB)
     c = conn.cursor()
-    c.execute(f'UPDATE {COCOA_TABLE} SET count={new_cocoa_count} WHERE user_id={user_id}')
+    c.execute(f"UPDATE {COCOA_TABLE} SET count={new_cocoa_count} WHERE user_id={user_id}")
     conn.commit()
     conn.close()
 
@@ -159,7 +157,7 @@ def print_database(database, table):
     conn = sqlite3.connect(database)
     c = conn.cursor()
     print('Printing SQLite DB')
-    for row in c.execute(f'SELECT * FROM {table}'):
+    for row in c.execute(f"SELECT * FROM {table}"):
         print(row)
     print('Done printing SQLite DB')
     conn.close()
@@ -176,7 +174,7 @@ if __name__ == '__main__':
         cocoa_db_uri = f'file:{pathname2url(COCOA_DB)}?mode=rwc'
         conn = sqlite3.connect(cocoa_db_uri, uri=True)
         c = conn.cursor()
-        c.execute(f'CREATE TABLE {COCOA_TABLE} (id int, count int)')
+        c.execute(f"CREATE TABLE {COCOA_TABLE} (id int, count int)")
         conn.commit()
         conn.close()
 
